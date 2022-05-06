fe5423cfeb32ba3b46f632f57b328b565d33b7d0
Sam Stoelinga <sammiestoel@gmail.com>
2022-05-06 15:05:01 -0700

    WIP check if pod is mutating

--- pspmutating.go
+++ pspmutating.go
@@ -1,17 +1,47 @@
 package pspmigrator
 
 import (
+	"context"
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/api/policy/v1beta1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
+
+func IsPodBeingMutatedByPSP(pod *v1.Pod, clientset *kubernetes.Clientset) (bool, error) {
+	// check if associated PSP object is using any mutating fields
+	// if yes then lookup ownerReferences and see if the field is actually mutating
+	// if no continue check for next pod
+	if pspName, ok := pod.ObjectMeta.Annotations["kubernetes.io/psp"]; ok {
+		pspObj, err := clientset.PolicyV1beta1().PodSecurityPolicies().Get(context.TODO(), pspName, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			return false, fmt.Errorf("PodSecurityPolicy %s not found: %w\n", pspName, err)
+		} else if err != nil {
+			return false, fmt.Errorf("Error getting PodSecurityPolicy %s: %w\n",
+				pspName, err)
+		} else if err != nil {
+			mutating, fields, annotations := IsPSPMutating(pspObj)
+			if mutating == false {
+				return false, nil
+			}
+			fmt.Println(fields, annotations)
+			// Lookup ownerReferences and compare pod spec with owner pod spec
+			//			fmt.Printf("%#v\n", pods.Items[0].ObjectMeta.OwnerReferences)
+			return true, nil
+		}
+	}
+	return false, nil
+
+}
 
 // IsPSPMutating checks wheter a PodSecurityPolicy is potentially mutating
 // pods. It returns true if one of the fields or annotations used in the
 // PodSecurityPolicy is suspected to be mutating pods. The field or annotations
 // that are suspected to be mutating are returned as well.
 func IsPSPMutating(pspObj *v1beta1.PodSecurityPolicy) (mutating bool, fields, annotations []string) {
-	// check if associated PSP object is using any mutating fields
-	// if yes then lookup ownerReferences and see if the field is actually mutating
-	// if no continue check for next pod
 	fields = make([]string, 0)
 	annotations = make([]string, 0)
 
